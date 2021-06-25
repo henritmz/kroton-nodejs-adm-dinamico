8724b5c420527416c256daeef5f3ff74761eb955
Dimitri <dimitrimuller@outlook.com>
2021-06-25 14:49:46 -0300

    updating utilities-color

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,5 +9,6 @@ router.get('/charts', HomeController.charts);
 router.get('/buttons', HomeController.buttons);
 router.get('/utilities-other', HomeController.others);
 router.get('/blank', HomeController.blank);
+router.get('/utilities-color', HomeController.color);
 
 module.exports = router;
