a80b3458be4b65caa18cd82a5f889a8c3641933c
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-07-02 14:21:21 -0300

    subindo middleware

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const HomeController = require("../controllers/home_controller")
+const LoginMiddleware = require("../middlewares/login")
 
-router.get('/', HomeController.index);
 router.get('/login', HomeController.login);
-router.get('/cards', HomeController.cards);
-router.get('/charts', HomeController.charts);
-router.get('/buttons', HomeController.buttons);
-router.get('/utilities-other', HomeController.others);
-router.get('/utilities-animation', HomeController.utilities_animation);
-router.get('/blank', HomeController.blank);
-router.get('/tables', HomeController.tables);
 router.get('/forgot-password', HomeController.forgotPassword);
 router.get('/register', HomeController.register);
-router.get('/utilities-border', HomeController.utilitiesBorder);
-router.get('/utilities-color', HomeController.color);
+
+router.get('/', LoginMiddleware, HomeController.index);
+router.get('/cards', LoginMiddleware, HomeController.cards);
+router.get('/charts', LoginMiddleware, HomeController.charts);
+router.get('/buttons', LoginMiddleware, HomeController.buttons);
+router.get('/utilities-other', LoginMiddleware, HomeController.others);
+router.get('/utilities-animation', LoginMiddleware, HomeController.utilities_animation);
+router.get('/blank', LoginMiddleware, HomeController.blank);
+router.get('/tables', LoginMiddleware, HomeController.tables);
+router.get('/utilities-border', LoginMiddleware, HomeController.utilitiesBorder);
+router.get('/utilities-color', LoginMiddleware, HomeController.color);
 
 module.exports = router;
